80d10603e7b291836aa5bd7244a27ea1bf0bb355
sergiocastillero <cf17sergio.casstillero@iesjoandaustria.org>
2021-03-16 00:22:00 +0000

    acabat, pasa proves prgtest

--- 03_04_elpenjat/Penjat.java
+++ 03_04_elpenjat/Penjat.java
@@ -11,6 +11,7 @@ public class Penjat{
         int intent = 10;
         String paraula = "recursos/paraules.txt";
         String utilitzades = "";
+        int comptadorlletres = 0;
         int paraulesencertades = 0;
         int encerts = 0;
         int fallades = 0;
@@ -21,7 +22,7 @@ public class Penjat{
         BufferedReader lecturaparaula = new BufferedReader(new FileReader(paraula));
             
             
-        System.out.println("Començem a jugar");
+        System.out.println("Comencem a jugar");
 
         String linia = lecturaparaula.readLine(); 
         char[] arraylletra;
@@ -33,13 +34,33 @@ public class Penjat{
                 numfigura = 0;
                 lletra = "*";
                 linia = lecturaparaula.readLine();
-                fallades++;
+                fallades++; 
+                comptadorlletres = 0;
+                if (null == linia) {
+                    System.out.println("No queden més paraules");
+                    System.out.println("Paraules jugades: " + (encerts + fallades + cancel));
+                    System.out.println("Paraules encertades: " + encerts);
+                    System.out.println("Paraules fallades: " + fallades);
+                    System.out.println("Paraules cancel·lades: " + cancel);
+                    System.out.println("Espero que t'hagis divertit");
+                break;
+            }
                 arraylletra = new char[linia.length()];
             }
             palabraoculta(lletra, linia, arraylletra);
             
             System.out.print("Utilitzades: ");
-            if (lletra != "*" && lletra.length() == 1) {
+            if (lletresutilitzades.contains(lletra.toUpperCase()) == true){
+                for(int y=0; y<lletresutilitzades.length(); y++){
+                    System.out.print(lletresutilitzades.charAt(y));
+                    if (y == lletresutilitzades.length()-2){
+                        System.out.print(" i ");
+                    }else if(y != lletresutilitzades.length()-1){
+                        System.out.print(", ");
+                    }
+                }  
+                System.out.println();
+                } else if (lletra != "*" && lletra.length() == 1) {
                 lletresutilitzades = lletresutilitzades + lletra.toUpperCase();
                 for(int y=0; y<lletresutilitzades.length(); y++){
                     System.out.print(lletresutilitzades.charAt(y));
@@ -50,6 +71,8 @@ public class Penjat{
                     }  
                 }
                 System.out.println();
+            }else if ((intent == 10 && lletra.length() > 1) || lletra == "*"){
+                System.out.println("cap");
             }else if(lletra.length() > 1){
                 for(int z=0; z<lletresutilitzades.length(); z++){
                     System.out.print(lletresutilitzades.charAt(z));
@@ -60,14 +83,14 @@ public class Penjat{
                     }  
                 }  
                 System.out.println();
-            }else {
-                System.out.println("cap");
             }
             System.out.println("Intents disponibles: "+ intent);
             System.out.println("Introdueix una lletra");
             lletra = Entrada.readLine();  
             
-            if (lletra.equals("prou")){
+            if (lletresutilitzades.contains(lletra.toUpperCase()) == true){
+                System.out.println("La lletra ja ha estat utilitzada");
+            }else if (lletra.toLowerCase().equals("prou")){
                 cancel++;
                 System.out.println("Paraules jugades: " + (encerts + fallades + cancel));
                 System.out.println("Paraules encertades: " + encerts);
@@ -75,34 +98,58 @@ public class Penjat{
                 System.out.println("Paraules cancel·lades: " + cancel);
                 System.out.println("Espero que t'hagis divertit");
                 break;
-            }else if(lletra.equals("glups")){
+            }else if(lletra.toLowerCase().equals("glups")){
                 intent = 11;
                 numfigura = -1;
                 lletra = "*";
                 lletresutilitzades = "";
+                comptadorlletres = 0;
                 linia = lecturaparaula.readLine();
                 cancel++;
+                 if (null == linia) {
+                    System.out.println("No queden més paraules");
+                    System.out.println("Paraules jugades: " + (encerts + fallades + cancel));
+                    System.out.println("Paraules encertades: " + encerts);
+                    System.out.println("Paraules fallades: " + fallades);
+                    System.out.println("Paraules cancel·lades: " + cancel);
+                    System.out.println("Espero que t'hagis divertit");
+                    break;
+                }
                 arraylletra = new char[linia.length()];
-            }else if ((lletra.isEmpty() || lletra.isBlank()) || !(lletra.charAt(0) >= 'a' && lletra.charAt(0) <= 'z') || lletra.length() > 1){
+            }else if (lletra.isEmpty() || lletra.isBlank() || !(lletra.toLowerCase().charAt(0) >= 'a' && lletra.toLowerCase().charAt(0) <= 'z') || lletra.length() > 1){
                 System.out.println("Error: cal una lletra entre 'a' i 'z', 'prou' o 'glups'");
             }
             
-            if (linia.contains(lletra) == false  && lletra.length() == 1) {
+            if ((linia.contains(lletra.toLowerCase()) == false)  && (lletra.length() == 1) && (lletresutilitzades.contains(lletra.toUpperCase()) == false)) {
                 intent--;
                 mostraFigura(numfigura);
                 numfigura++;
-            } else if (linia.contains(lletra) == true  && lletra.length() == 1 && lletresutilitzades.contains(lletra) == false){
-                paraulesencertades++;
+            } else if ((linia.contains(lletra.toLowerCase()) == true)  && (lletra.length() == 1) && (lletresutilitzades.contains(lletra.toUpperCase()) == false)){
+                for (int x=0; x < linia.length(); x++) {
+                     if (linia.charAt(x) == lletra.toLowerCase().charAt(0)) {
+                        comptadorlletres++;
+                     }
+                 }
             }
-            if (paraulesencertades == linia.length()){
+            if (comptadorlletres == linia.length()){
                 encerts++;
-                System.out.println("Has encertat! La paraula era " + linia);
-                linia = lecturaparaula.readLine();
-                arraylletra = new char[linia.length()];
+                comptadorlletres = 0;
                 intent = 10;
                 numfigura = 0;
                 lletra = "*";
                 lletresutilitzades = "";
+                System.out.println("Has encertat! La paraula era " + linia);
+                linia = lecturaparaula.readLine();
+                if (null == linia) {
+                    System.out.println("No queden més paraules");
+                    System.out.println("Paraules jugades: " + (encerts + fallades + cancel));
+                    System.out.println("Paraules encertades: " + encerts);
+                    System.out.println("Paraules fallades: " + fallades);
+                    System.out.println("Paraules cancel·lades: " + cancel);
+                    System.out.println("Espero que t'hagis divertit");
+                    break;
+                }
+                arraylletra = new char[linia.length()];
             }
         }
         lecturaparaula.close();
@@ -112,20 +159,23 @@ public class Penjat{
             String figura = "recursos/figura" + numfigura + ".txt";
             BufferedReader mostrafigura = new BufferedReader(new FileReader(figura));
             while ((figura = mostrafigura.readLine()) != null) {
-                System.out.println(figura.strip());
+                System.out.println(figura);
             } 
             mostrafigura.close(); 
+            if (numfigura == 9){
+                System.out.println("Has mort");
+            }
         }
     }
     public static void palabraoculta(String lletra, String linia, char[] arraylletra){
         if (lletra.equals("*")){
             for (int j = 0; j < linia.length(); j++){
-                arraylletra[j] = lletra.charAt(0);
+                arraylletra[j] = lletra.toLowerCase().charAt(0);
             }   
         }
         System.out.print("Paraula: ");
         for (int i=0; i<linia.length(); i++){
-            if ((linia.charAt(i) == lletra.charAt(0)) && lletra.length() == 1) {
+            if ((linia.charAt(i) == lletra.toLowerCase().charAt(0)) && lletra.length() == 1) {
                  arraylletra[i] = linia.charAt(i);
             }
             System.out.print(arraylletra[i]);
